0d4c4315a6e26e8ced6154ee92b998a35936bc24
demokan01 <dmkn01@hotmail.com>
2021-08-03 01:22:23 +0300

    Add project files.

---
+++ AspNetCoreMvcECommerce/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcECommerce.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
