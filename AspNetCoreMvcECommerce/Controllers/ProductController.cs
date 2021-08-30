7d1257c02a84929dd1a4202d7325f8aaf8f20460
demokan01 <dmkn01@hotmail.com>
2021-08-30 10:58:14 +0300

    Kategorilere Göre Ürün Listeleme Yapıldı

--- AspNetCoreMvcECommerce/Controllers/ProductController.cs
+++ AspNetCoreMvcECommerce/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace AspNetCoreMvcECommerce.Controllers
 {
@@ -27,6 +28,17 @@ namespace AspNetCoreMvcECommerce.Controllers
             ViewBag.ProductImages = product.Photos.Where(p => p.Status).ToList();
             ViewBag.RelatedProducts = db.Products.Where(p => p.CategoryId == product.CategoryId && p.Id != id && p.Status).ToList();
             return View("Details");
+        }
+
+        [Route("category/{id}")]
+        public IActionResult Category(int id,int? page)
+        {
+            var pageNumber = page ?? 1;
+            var category = db.Categories.Find(id);
+            ViewBag.Category = category;
+            ViewBag.CountProducts = category.Products.Count(p => p.Status);
+            ViewBag.Products = category.Products.Where(p => p.Status).ToList().ToPagedList(pageNumber,9);
+            return View("Category");
         }
     }
 }
